d6a79deda9d5dfe85c2795965c1ab2bf6fdeade6
Cheng Shi <cheng.shi@georgebrown.ca>
2025-03-26 00:21:33 -0700

    declare variables

--- A2_iOS_Cheng_101362082/ViewController.swift
+++ A2_iOS_Cheng_101362082/ViewController.swift
@@ -10,6 +10,12 @@ import coreData
 
 class ViewController: UIViewController{
 
+    var products: [Product] = []
+    var filteredProducts: [Product] = []
+    var isSearching = false //track search state
+        
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
