20af040b648593dc91bd365a8fae345e02aafb52
Cheng Shi <cheng.shi@georgebrown.ca>
2025-03-26 00:58:28 -0700

    fixed bug

--- A2_iOS_Cheng_101362082/ViewController.swift
+++ A2_iOS_Cheng_101362082/ViewController.swift
@@ -2,101 +2,113 @@
 //  ViewController.swift
 //  A2_iOS_Cheng_101362082
 //
-//  Created by Heather Shi on 2025-03-25.
+//  Created by Cheng Shi on 2025-03-25.
 //
 
 import UIKit
-import coreData
+import CoreData
 
 class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
-    
-    var products: [Product] = []
-    var filteredProducts: [Product] = []
-    var isSearching = false //track search state
-    
+
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var productNameTextField: UITextField!
+    @IBOutlet weak var productDescriptionTextField: UITextField!
+    @IBOutlet weak var productPriceTextField: UITextField!
+    @IBOutlet weak var productProviderTextField: UITextField!
+
+    var products: [Product] = [] // All products from Core Data
+    var filteredProducts: [Product] = [] // Search results
+    var isSearching = false // Track search state
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        func setupSearchBar(){
-            let searchBar = UISearchBar()
-            searchBar.placeholder = "Search by Name or Description"
-            searchBar.delegate = self
-            self.navigationItem.titleView = searchBar
-        }
-        
-        func fetchProducts() {
-            let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
-            
-            do {
-                let fetchedProducts = try PersistenceController.shared.container.viewContext.fetch(fetchRequest)
-                self.products = fetchedProducts
-                self.filteredProducts = fetchedProducts // Default to all products
-                tableView.reloadData()
-            } catch {
-                print("Error fetching products: \(error)")
-            }
-        }
-        
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            if searchText.isEmpty {
-                isSearching = false
-                filteredProducts = products // Show all products when search bar is empty
-            } else {
-                isSearching = true
-                let predicate = NSPredicate(format: "productName CONTAINS[cd] %@ OR productDescription CONTAINS[cd] %@", searchText, searchText)
-                
-                let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
-                fetchRequest.predicate = predicate
-                
-                do {
-                    filteredProducts = try PersistenceController.shared.container.viewContext.fetch(fetchRequest)
-                } catch {
-                    print("Error filtering products: \(error)")
-                }
-            }
-            tableView.reloadData()
-        }
         
         setupSearchBar()
+        setupTableView()
         fetchProducts()
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return isSearching ? filteredProducts.count : products.count
-        }
+    }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
-            let product = isSearching ? filteredProducts[indexPath.row] : products[indexPath.row]
-            
-            cell.textLabel?.text = product.productName
-            cell.detailTextLabel?.text = product.productDescription
-            
-            return cell
-        }
+    //  Set up UISearchBar
+    func setupSearchBar() {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search by Name or Description"
+        searchBar.delegate = self
+        self.navigationItem.titleView = searchBar
+    }
+
+    // Set up UITableView
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    // Fetch products from Core Data
+    func fetchProducts() {
+        let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
         
-        @IBAction func saveProduct(_ sender: Any) {
-            let context = PersistenceController.shared.container.viewContext
-            let newProduct = Product(context: context)
+        do {
+            products = try PersistenceController.shared.container.viewContext.fetch(fetchRequest)
+            filteredProducts = products // Default to all products
+            tableView.reloadData()
+        } catch {
+            print("Error fetching products: \(error)")
+        }
+    }
+
+    // Search logic
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            isSearching = false
+            filteredProducts = products // Show all products when search bar is empty
+        } else {
+            isSearching = true
+            let predicate = NSPredicate(format: "productName CONTAINS[cd] %@ OR productDescription CONTAINS[cd] %@", searchText, searchText)
             
-            newProduct.productName = productNameTextField.text
-            newProduct.productDescription = productDescriptionTextField.text
-            newProduct.productPrice = Decimal(string: productPriceTextField.text ?? "0") ?? 0
-            newProduct.productProvider = productProviderTextField.text
+            let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+            fetchRequest.predicate = predicate
             
             do {
-                try context.save()
-                navigationController?.popViewController(animated: true)
+                filteredProducts = try PersistenceController.shared.container.viewContext.fetch(fetchRequest)
             } catch {
-                print("Failed to save new product: \(error)")
+                print("Error filtering products: \(error)")
             }
+        }
+        tableView.reloadData()
+    }
 
+    // UITableView DataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isSearching ? filteredProducts.count : products.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let product = isSearching ? filteredProducts[indexPath.row] : products[indexPath.row]
+
+        cell.textLabel?.text = product.productName
+        cell.detailTextLabel?.text = product.productDescription
+
+        return cell
+    }
+
+    //  Save a new product to Core Data
+    @IBAction func saveProduct(_ sender: Any) {
+        let context = PersistenceController.shared.container.viewContext
+        let newProduct = Product(context: context)
+
+        newProduct.productName = productNameTextField.text
+        newProduct.productDescription = productDescriptionTextField.text
+        newProduct.productPrice = Decimal(string: productPriceTextField.text ?? "0") ?? 0
+        newProduct.productProvider = productProviderTextField.text
+
+        do {
+            try context.save()
+            fetchProducts() // Refresh the list after saving
+
+
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Failed to save new product: \(error)")
         }
     }
 }
-
-
-
-    
